f1f4a82c26104229f32b25d9a1bbd0a54ec6bea5
STC-s21014 <s21014@suntech.jp>
2022-07-26 16:01:34 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mycostainlayout/OrderConfirmDialogFragment.java
@@ -0,0 +1,8 @@
+package com.example.mycostainlayout;
+
+import androidx.fragment.app.FragmentManager;
+
+public class OrderConfirmDialogFragment {
+    public void show(FragmentManager supportFragmentManager, String orderConfirmDialogFragment) {
+    }
+}
